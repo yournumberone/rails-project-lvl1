e1bd8a0917e0b45830b1f33fd82ada12bffbb059
theworking <marsthegod@protonmail.com>
2022-02-17 01:20:14 +0300

    generate html tags from method build

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
-require_relative "hexlet_code/version"
+require_relative 'hexlet_code/version'
 
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Tag
+
+    def self.build(tag, params = {})
+      html_attributes = params.to_s.delete('{}:,').gsub('=>', '=').gsub('"', "&quot;")
+      if block_given?
+        "<#{tag} #{html_attributes}>#{yield}</#{tag}>"
+      else
+        "<#{tag} #{html_attributes}>"
+      end
+    end
+
+  end
+
 end
