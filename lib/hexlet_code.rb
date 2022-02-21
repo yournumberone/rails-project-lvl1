c0a8b4a4b6185164865ee26fcaa0df023132550d
theworking <marsthegod@protonmail.com>
2022-02-22 01:11:40 +0300

    upd formbuilder

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,28 +1,41 @@
 # frozen_string_literal: true
-
 require_relative 'hexlet_code/version'
+
+
 
 module HexletCode
   class Error < StandardError; end
 
-  def self.form_for(object, params = {})
-    object_name = object.to_s
-    url = params.include?(:url) ? params[:url].gsub('"', "'") : "#"
-    action="action='#{url}'"
-    "<form #{action} method='post'>\n</form>"
-  end
-  class Tag
 
-    def self.build(tag, params = {})
-      html_attributes = params.to_s.delete('{}:,').gsub('=>', '=').gsub('"', "'")
-      if block_given?
-        "<#{tag} #{html_attributes}>#{yield}</#{tag}>"
-      else
-        "<#{tag} #{html_attributes}>"
+  def self.form_for(object, options = {}, &block)
+    url = options.include?(:url) ? options[:url].gsub('"', "'") : "#"
+    action="action='#{url}'"
+    result = ["<form #{action} method='post'><br>"]
+    object.define_singleton_method(:input) do |field, type = {}|
+      response = object.public_send(field)
+      if type == {:as=>:text}
+        result.push("<textarea cols='20' rows='40' name='#{field}'>"); result.push("#{response}</textarea><br>")
+      elsif type.empty?
+        result.push("<input name='#{field}' type='text' value="); result.push("'#{response}'><br>")
       end
     end
+    block.call object
+    result.push("</form>")
+    p result.join
+
+  end
 
 
+  class Tag
+    # def self.input(attribute)
+    #   puts "hi"
+    #   puts attribute
+    # end
+
+    def self.build(tag, options = {})
+      html_attributes = options.to_s.delete('{}:,').gsub('=>', '=').gsub('"', "'")
+      block_given? ? "<#{tag} #{html_attributes}>#{block.call}</#{tag}>" : "<#{tag} #{html_attributes}>"
+    end
 
   end
 
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -1,18 +1,31 @@
 # frozen_string_literal: true
-
-require "test_helper"
+require 'test_helper'
 
 class TestHexletCode < Minitest::Test
   User = Struct.new(:name, :job, keyword_init: true)
-  @user = User.new name: 'Bobby'
+
+  def setup
+    @user = User.new name: 'Bobby', job: 'Hexlet'
+  end
+
+
+
+  def test_s
+    @html = HexletCode.form_for @user, url: "/blabla" do |f|
+      f.input :name
+      f.input :job, as: :text
+    end
+  end
 
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
 
   def test_that_returns_form
-    assert_equal(HexletCode.form_for(@user) {}, "<form action='#' method='post'>\n</form>")
-    assert_equal(HexletCode.form_for(@user, url: '/posts') {}, "<form action='/posts' method='post'>\n</form>")
+    assert_equal(HexletCode.form_for(@user) {}, "<form action='#' method='post'><br></form>")
+    assert_equal(HexletCode.form_for(@user, url: '/posts') {}, "<form action='/posts' method='post'><br></form>")
+
+    puts @html
   end
 
 end
